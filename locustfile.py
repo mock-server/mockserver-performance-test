51e9330344eeabd895707dda4d0a04ef7a790f04
James Bloom <jamesdbloom@gmail.com>
2018-01-21 12:53:31 +0000

    swapping locust for apache-bench, wrk or wrk2 as none of them are reliable enough

---
+++ locustfile.py
@@ -0,0 +1,14 @@
+from locust import HttpLocust, TaskSet, task
+import locust.stats
+locust.stats.CONSOLE_STATS_INTERVAL_SEC = 60
+
+class UserBehavior(TaskSet):
+    @task(1)
+    def simple(self):
+        self.client.get("/simple")
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    # min_wait = 5000
+    # max_wait = 9000
--- performance_test.sh
+++ performance_test.sh
@@ -13,71 +13,59 @@ function runCommand {
 
 sleep 2
 echo "+++ Create Expectation"
-curl -v -s -X PUT http://$MOCKSERVER_HOST/expectation -d '{
-    "httpRequest": {
-        "path": "/simple"
+curl -v -s -X PUT http://$MOCKSERVER_HOST/expectation -d '[
+    {
+        "httpRequest": {
+            "path": "/not_simple"
+        },
+        "httpResponse": {
+            "statusCode": 200,
+            "body": "some not simple response"
+        },
+        "times": {
+            "unlimited": true
+        }
     },
-    "httpResponse": {
-        "statusCode": 200,
-        "body": "some response"
+    {
+        "httpRequest": {
+            "method": "POST",
+            "path": "/simple"
+        },
+        "httpResponse": {
+            "statusCode": 200,
+            "body": "some simple POST response"
+        },
+        "times": {
+            "unlimited": true
+        }
     },
-    "times": {
-        "unlimited": true
+    {
+        "httpRequest": {
+            "path": "/simple"
+        },
+        "httpResponse": {
+            "statusCode": 200,
+            "body": "some simple response"
+        },
+        "times": {
+            "unlimited": true
+        }
     }
-}'
+]'
 
 echo "+++ JVM warm up"
-#runCommand "wrk --latency -t2 -c100 -d10s -R2000 https://$MOCKSERVER_HOST/simple"
-runCommand "ab -n 100 -c 10 http://$MOCKSERVER_HOST/simple"
-#runCommand "wrk --latency -t2 -c100 -d10s -R2000 https://$MOCKSERVER_HOST/simple"
-runCommand "ab -n 100 -c 10 https://$MOCKSERVER_HOST/simple"
+runCommand "locust --loglevel=INFO --no-web --only-summary --csv=10c_noTLS -c 10 -r 10 -n 100 --host=http://$MOCKSERVER_HOST"
 
-#echo "+++ HTTP 1 request/sec"
-#runCommand "wrk --latency -t2 -c100 -d18s -R1 http://$MOCKSERVER_HOST/simple"
-#echo "+++ HTTP 5 request/sec"
-#runCommand "wrk --latency -t2 -c100 -d18s -R5 http://$MOCKSERVER_HOST/simple"
-#echo "+++ HTTP 10 request/sec"
-#runCommand "wrk --latency -t2 -c100 -d18s -R10 http://$MOCKSERVER_HOST/simple"
-#echo "+++ HTTP 100 request/sec"
-#runCommand "wrk --latency -t2 -c100 -d18s -R100 http://$MOCKSERVER_HOST/simple"
-#echo "+++ HTTP 1000 request/sec"
-#runCommand "wrk --latency -t2 -c100 -d18s -R1000 http://$MOCKSERVER_HOST/simple"
-echo "+++ HTTP 1 connections"
-runCommand "ab -n 10000 -c 1 http://$MOCKSERVER_HOST/simple"
-echo "+++ HTTP 5 connections"
-runCommand "ab -n 10000 -c 5 http://$MOCKSERVER_HOST/simple"
-echo "+++ HTTP 10 connections"
-runCommand "ab -n 10000 -c 10 http://$MOCKSERVER_HOST/simple"
-echo "+++ HTTP 50 connections"
-runCommand "ab -n 10000 -c 50 http://$MOCKSERVER_HOST/simple"
-echo "+++ HTTP 100 connections"
-runCommand "ab -n 10000 -c 100 http://$MOCKSERVER_HOST/simple"
-echo "+++ HTTP 150 connections"
-runCommand "ab -n 10000 -c 150 http://$MOCKSERVER_HOST/simple"
-echo "+++ HTTP 250 connections"
-runCommand "ab -n 10000 -c 250 http://$MOCKSERVER_HOST/simple"
+echo "+++ HTTP"
+runCommand "locust --loglevel=INFO --no-web --only-summary --csv=1c_noTLS -c 1 -r 1 -n 10 --host=http://$MOCKSERVER_HOST"
+runCommand "locust --loglevel=INFO --no-web --only-summary --csv=10c_noTLS -c 10 -r 10 -n 100 --host=http://$MOCKSERVER_HOST"
+runCommand "locust --loglevel=INFO --no-web --only-summary --csv=100c_noTLS -c 100 -r 100 -n 1000 --host=http://$MOCKSERVER_HOST"
+runCommand "locust --loglevel=INFO --no-web --only-summary --csv=200c_noTLS -c 200 -r 200 -n 20000 --host=http://$MOCKSERVER_HOST"
 
-#echo "+++ HTTPS 1 request/sec"
-#runCommand "wrk --latency -t2 -c100 -d18s -R1 https://$MOCKSERVER_HOST/simple"
-#echo "+++ HTTPS 5 request/sec"
-#runCommand "wrk --latency -t2 -c100 -d18s -R5 https://$MOCKSERVER_HOST/simple"
-#echo "+++ HTTPS 10 request/sec"
-#runCommand "wrk --latency -t2 -c100 -d18s -R10 https://$MOCKSERVER_HOST/simple"
-#echo "+++ HTTPS 100 request/sec"
-#runCommand "wrk --latency -t2 -c100 -d18s -R100 https://$MOCKSERVER_HOST/simple"
-#echo "+++ HTTPS 1000 request/sec"
-#runCommand "wrk --latency -t2 -c100 -d18s -R1000 https://$MOCKSERVER_HOST/simple"
-echo "+++ HTTPS 1 connections"
-runCommand "ab -n 10000 -c 1 https://$MOCKSERVER_HOST/simple"
-echo "+++ HTTPS 5 connections"
-runCommand "ab -n 10000 -c 5 https://$MOCKSERVER_HOST/simple"
-echo "+++ HTTPS 10 connections"
-runCommand "ab -n 10000 -c 10 https://$MOCKSERVER_HOST/simple"
-echo "+++ HTTPS 50 connections"
-runCommand "ab -n 10000 -c 50 https://$MOCKSERVER_HOST/simple"
-echo "+++ HTTPS 100 connections"
-runCommand "ab -n 10000 -c 100 https://$MOCKSERVER_HOST/simple"
-echo "+++ HTTPS 150 connections"
-runCommand "ab -n 10000 -c 150 https://$MOCKSERVER_HOST/simple"
-echo "+++ HTTPS 250 connections"
-runCommand "ab -n 10000 -c 250 https://$MOCKSERVER_HOST/simple"
+echo "+++ HTTPS"
+runCommand "locust --loglevel=INFO --no-web --only-summary --csv=1c_TLS -c 1 -r 1 -n 10 --host=http://$MOCKSERVER_HOST"
+runCommand "locust --loglevel=INFO --no-web --only-summary --csv=10c_TLS -c 10 -r 10 -n 100 --host=http://$MOCKSERVER_HOST"
+runCommand "locust --loglevel=INFO --no-web --only-summary --csv=100c_TLS -c 100 -r 100 -n 1000 --host=http://$MOCKSERVER_HOST"
+runCommand "locust --loglevel=INFO --no-web --only-summary --csv=200c_TLS -c 200 -r 200 -n 20000 --host=http://$MOCKSERVER_HOST"
+
+
