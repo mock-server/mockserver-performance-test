cc8eda583e0da8a4df3e5c115331d51f5aab430b
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2019-11-01 21:20:47 +0000

    fixed error in performance script

--- performance_test.sh
+++ performance_test.sh
@@ -74,10 +74,10 @@ curl -v -s -X PUT http://$MOCKSERVER_HOST/expectation -d '[
 ]'
 
 echo "+++ JVM warm up"
-runCommand "locust --loglevel=ERROR --no-web --only-summary -c 10 -r 10 -n 100 --host=http://$MOCKSERVER_HOST"
+runCommand "locust --loglevel=ERROR --no-web --only-summary -c 10 -r 10 -t 10 --host=http://$MOCKSERVER_HOST"
 
 echo "+++ HTTP"
 for count in 10 100 200 300 400 500 600 700 800 900 1000
 do
-    runCommand "locust --loglevel=INFO --no-web --only-summary --csv=1c_noTLS -c $count -r $count -n $(($count*60)) --host=http://$MOCKSERVER_HOST"
+    runCommand "locust --loglevel=INFO --no-web --only-summary --csv=1c_noTLS -c $count -r $count -t $count --host=http://$MOCKSERVER_HOST"
 done
