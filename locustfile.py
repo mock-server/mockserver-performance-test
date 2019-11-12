2427ebacb5e6a28b22c67c0c93e4e3c67a2c3d04
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2019-11-12 06:15:05 +0000

    disabled TLS certificate verification

--- locustfile.py
+++ locustfile.py
@@ -1,15 +1,20 @@
 from locust import HttpLocust, TaskSet, task
 import locust.stats
 locust.stats.CONSOLE_STATS_INTERVAL_SEC = 60
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+
+# Suppress only the single warning from urllib3 needed.
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 class UserBehavior(TaskSet):
     @task
     def simple(self):
-        self.client.get("/simple")
+        self.client.get("/simple", verify=False)
 
-    @task
-    def forward(self):
-        self.client.get("/forward")
+    # @task
+    # def forward(self):
+    #     self.client.get("/forward")
 
 
 class WebsiteUser(HttpLocust):
