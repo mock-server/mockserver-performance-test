ab03b04f54fef798894485e4ae1d904af0516c66
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2020-06-08 07:09:36 +0100

    cleaned script

--- local_performance_test.sh
+++ local_performance_test.sh
@@ -4,8 +4,9 @@
 
 ulimit -S -n 49152
 
+#    -Dmockserver.maxExpectations=$count \
+
 #for count in 0 10 50 100 500 1000 2500 5000 7500 10000 15000 20000 25000 30000 35000 40000 45000 50000
-#for count in 0
 #do
   curl -v -k -X PUT http://localhost:1080/stop || true
   sleep 1
@@ -17,5 +18,3 @@ ulimit -S -n 49152
   sleep 2
   MOCKSERVER_HOST=localhost:1080 ./performance_test_short.sh
 #done
-
-#    -Dmockserver.maxExpectations=$count \
